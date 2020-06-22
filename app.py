35eda3a1234e8563365679c158104edb5ba64b3c
Gareth1980 <garethbuckley@hotmail.com>
2020-06-22 21:01:22 +0000

    added edit, delete and add buttons and wired them up. I binded the data to the edit question form.

--- app.py
+++ app.py
@@ -25,6 +25,35 @@ def get_questions():
 def add_question():
     return render_template('addquestion.html', categories=mongo.db.categories.find())
 
+@app.route('/insert_question', methods=['POST'])
+def insert_question():
+    question_and_answer = mongo.db.question_and_answer
+    question_and_answer.insert_one(request.form.to_dict())
+    return redirect(url_for('get_questions'))
+
+@app.route('/edit_question/<question_and_answer_id>')
+def edit_question(question_and_answer_id):
+    the_question =  mongo.db.question_and_answer.find_one({"_id": ObjectId(question_and_answer_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editquestion.html', question_and_answer=the_question,
+                           categories=all_categories)
+
+@app.route('/update_question/<question_and_answer_id>', methods=['POST'])                           
+def update_question(question_and_answer_id):
+    question_and_answer = mongo.db.question_and_answer
+    question_and_answer.update( {'_id': ObjectId(question_and_answer_id)},
+    {
+        'category_name':request.form.get('category_name'),
+        'question':request.form.get('question'),
+        'answer': request.form.get('answer')
+    })
+    return redirect(url_for('get_questions'))
+
+@app.route('/delete_question/<question_and_answer_id>')
+def delete_question(question_and_answer_id):
+    mongo.db.question_and_answer.remove({'_id': ObjectId(question_and_answer_id)})
+    return redirect(url_for('get_questions'))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
